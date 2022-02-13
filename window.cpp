0061af19990dae835066aa3eedff44bcca8df751
KtoToOchenStrannij <sorokinaanya69@mail.ru>
2022-02-13 14:23:50 +0300

    теперь всё ок

--- window.cpp
+++ window.cpp
@@ -9,9 +9,9 @@ window::window(QWidget *parent): QWidget(parent)
       for (int i=0; i<9; i++) {
        for (int j=0; j<9; j++) {
 
-         SButton btn;
-         btn.setFixedSize(40, 40);
-         grid->addWidget(&btn, i, j);
+         SButton* btn = new SButton;
+         btn->setFixedSize(40, 40);
+         grid->addWidget(btn, i, j);
        }
       }
 
